b0ab38d0a01f6931ee2f960251da84243db4e5ea
Brajan <brajan.fitas@gmail.com>
2020-09-19 22:25:17 +0200

    eks 0.9

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -52,7 +52,6 @@ resource "aws_security_group" "cluster" {
 }
 
 resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
-  cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
